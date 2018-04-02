e7a8d66f6bbd1d4d24c6da09110b1a322b14e30e
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-04-02 21:04:08 +0300

    add tge-info block

--- www/js/main.js
+++ www/js/main.js
@@ -6,4 +6,11 @@
     $('#headerMenu').collapse('toggle');
   });
 })();
+'use strict';
+
+// (() => {
+$(function () {
+  $('[data-toggle="tooltip"]').tooltip();
+});
+// })();
 //# sourceMappingURL=main.js.map
