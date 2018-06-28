cfd8060bb2dca589cbdeccf5d99924d2e91483e7
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-06-28 10:19:40 +0300

    update features block

--- src/scripts/lib/more-info.js
+++ src/scripts/lib/more-info.js
@@ -1,23 +1,23 @@
-(() => {
-  const els = {
-    open: document.getElementById('more-info-open'),
-    $features: $('.features'),
-    close: document.getElementById('more-info-close'),
-    $data: $('[data-more-info]'),
-  };
-
-  els.$data.toggle();
-
-  $( els.open ).click(() => {
-    els.$features.toggleClass('features--state--open');
-    els.$data.toggle();
-    $( els.open ).toggle();
-  });
-
-  $( els.close ).click(() => {
-    els.$features.toggleClass('features--state--open');
-    els.$data.toggle();
-    $( els.open ).toggle();
-  });
-
-})();
+// (() => {
+//   const els = {
+//     open: document.getElementById('more-info-open'),
+//     $features: $('.features'),
+//     close: document.getElementById('more-info-close'),
+//     $data: $('[data-more-info]'),
+//   };
+//
+//   els.$data.toggle();
+//
+//   $( els.open ).click(() => {
+//     els.$features.toggleClass('features--state--open');
+//     els.$data.toggle();
+//     $( els.open ).toggle();
+//   });
+//
+//   $( els.close ).click(() => {
+//     els.$features.toggleClass('features--state--open');
+//     els.$data.toggle();
+//     $( els.open ).toggle();
+//   });
+//
+// })();
