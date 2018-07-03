5f9f05802434a5f48e95e71af60fa6b2fbbf7287
Bazhenov Eugen <wtf.czabe@gmail.com>
2018-07-03 16:03:37 +0300

    final configuration of scroll revealling animation

--- src/scripts/lib/more-info.js
+++ src/scripts/lib/more-info.js
@@ -1,23 +1,19 @@
-// (() => {
-//   const els = {
-//     open: document.getElementById('more-info-open'),
-//     $features: $('.features'),
-//     close: document.getElementById('more-info-close'),
-//     $data: $('[data-more-info]'),
-//   };
-//
-//   els.$data.toggle();
-//
-//   $( els.open ).click(() => {
-//     els.$features.toggleClass('features--state--open');
-//     els.$data.toggle();
-//     $( els.open ).toggle();
-//   });
-//
-//   $( els.close ).click(() => {
-//     els.$features.toggleClass('features--state--open');
-//     els.$data.toggle();
-//     $( els.open ).toggle();
-//   });
-//
-// })();
+(() => {
+  const config = {
+    btn: 'more-info-btn',
+    name: 'more-info',
+  };
+
+  // $( els.open ).click(() => {
+  //   els.$features.toggleClass('features--state--open');
+  //   els.$data.toggle();
+  //   $( els.open ).toggle();
+  // });
+  //
+  // $( els.close ).click(() => {
+  //   els.$features.toggleClass('features--state--open');
+  //   els.$data.toggle();
+  //   $( els.open ).toggle();
+  // });
+
+})();
--- src/scripts/lib/scroll-animations.js
+++ src/scripts/lib/scroll-animations.js
@@ -54,15 +54,16 @@ class ScrollHandler {
 }
 
 class ScrollRevealElement {
-  constructor(agent, element, meta, config) {
+  constructor(agent, element, meta, config, container) {
     this.element = element;
     this.meta = meta || {};
     this.config = config || {};
     this.sr = agent;
+    this.container = container;
   }
 
   reveal() {
-    this.sr.reveal(this.element, this.config);
+    this.sr.reveal(this.element, this.config, this.meta.groupDelay || null);
     return this;
   }
 
@@ -76,6 +77,10 @@ class ScrollRevealElement {
 
   getElement() {
     return this.element;
+  }
+
+  getContainer() {
+    return this.container;
   }
 
   set(config) {
@@ -117,11 +122,6 @@ class ScrollRevealElement {
 
 (() => {
   const sr = ScrollReveal({
-    reset: true,
-    scale: 1,
-    duration: 750,
-    distance: '125px',
-    viewFactor: 0.1,
     // viewOffset: { top: 64 }
   });
 
@@ -130,43 +130,150 @@ class ScrollRevealElement {
   const config = {
     selector: '[data-scroll-reveal]',
     attrs: {
-      origin: 'data-origin',
-      changeOrigin: 'data-change-origin'
+      origin: {
+        name: 'data-origin',
+        data: 'origin',
+        scope: 'config'
+      },
+      opacity: {
+        name: 'data-opacity',
+        data: 'opacity',
+        scope: 'config'
+      },
+      viewFactor: {
+        name: 'data-view-factor',
+        data: 'viewFactor',
+        scope: 'config'
+      },
+      reset: {
+        name: 'data-reset',
+        data: 'reset',
+        scope: 'config'
+      },
+      distance: {
+        name: 'data-distance',
+        data: 'distance',
+        scope: 'config'
+      },
+      scale: {
+        name: 'data-scale',
+        data: 'scale',
+        scope: 'config'
+      },
+      duration: {
+        name: 'data-duration',
+        data: 'duration',
+        scope: 'config'
+      },
+      changeOrigin: {
+        name: 'data-change-origin',
+        data: 'changeOrigin',
+        scope: 'meta'
+      },
+      groupDelay: {
+        name: 'data-group-delay',
+        data: 'groupDelay',
+        scope: 'meta',
+      },
+      groupName: {
+        name: 'data-group-name',
+        data: 'groupName',
+        scope: 'meta',
+      },
+      groupContainer: {
+        name: 'data-group-container',
+        data: 'groupContainer',
+      }
     },
     default: {
       origin: 'bottom',
+      viewFactor: 0.1,
+      opacity: 0,
       changeOrigin: true,
+      reset: true,
+      distance: '125px',
+      scale: 1,
+      duration: 750,
+    },
+    getNames: function(fn) {
+      return Object.keys(this.attrs).filter(fn || (k => k));
+    },
+    getMetaNames: function() {
+      return this.getNames(k => this.attrs[k].scope === 'meta');
+    },
+    getConfigNames: function() {
+      return this.getNames(k => this.attrs[k].scope === 'config');
     },
     parser: function(el) {
-      const attrsNames = Object.keys(this.attrs);
+      const metaNames = this.getMetaNames();
+      const configNames = this.getConfigNames();
 
-      const meta = attrsNames.reduce((meta, attrName) => {
-        const attr = $( el ).attr(this.attrs[attrName]);
+      const element = el instanceof Array ? el[0] : el;
 
-        return { ...meta, [attrName] : (attr ? attr : this.default[attrName]) };
+      const pack = (names) => names.reduce((meta, attrName) => {
+        const attr = $( element ).data(this.attrs[attrName].data);
+
+        return { ...meta, [attrName] : (attr !== undefined ? attr : this.default[attrName]) };
       }, {});
 
-      return meta;
+      return { meta: pack(metaNames), config: pack(configNames) };
     },
+    parseArrays: (arr, fn) => {
+      const rec = (acc, arr1, arr2) => {
+        if(acc.length === 0) return { with: arr1, without: arr2 };
+
+        const [first, ...rest] = acc;
+
+        return fn(first) ? rec(rest, [...arr1, first], arr2) : rec(rest, arr1, [...arr2, first]);
+      }
+
+      return rec(arr, [], []);
+    },
+    breakIntoGroups: function(elements) {
+      const rec = (acc, elements) => {
+        if(elements.length === 0) return acc;
+
+        const [first, ...rest] = elements;
+
+        const groupName = $( first ).data( this.attrs.groupName.data );
+
+        if(groupName) {
+          const { with: groupRest, without: filteredRest } = this.parseArrays(rest, e => $( e ).data(this.attrs.groupName.data) === groupName);
+
+          return rec([...acc, [first, ...groupRest]], filteredRest);
+        };
+
+        return rec([...acc, first], rest);
+      }
+
+      return rec([], elements);
+    }
   };
 
-  const elements = $(config.selector).get().map(e => {
-    const meta = config.parser(e);
+  const elements = config.breakIntoGroups($(config.selector).get()).map(e => {
+    const { meta, config: conf } = config.parser(e);
 
-    return new ScrollRevealElement(sr, e, meta);
+    const containerAttr = `[${config.attrs.groupContainer.name}="${meta.groupName}"]`;
+    const container = meta.groupName ? $( containerAttr ).get(0) : e;
+
+    return new ScrollRevealElement(sr, e, meta, conf, container);
   });
 
   elements.forEach(e => {
-    e.bottom();
+    e.reveal();
   });
 
   scroll.setCallback(function(ev) {
     const { bottom } = this.getMeta();
 
     elements.forEach(e => {
+      const meta = e.getMeta();
+
+      if(!meta.changeOrigin) return;
+
       const { origin } = e.getConfig();
 
-      const domEl = e.getElement();
+      const domEl = e.getContainer();
       const { y, height } = domEl.getBoundingClientRect();
       const middle = Math.round(y + height / 2);
 
